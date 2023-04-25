5c18f8b6f57fea0a0e04506841f0dd0416bba483
Arhises <roma1039@gmail.com>
2023-04-25 15:24:05 +0300

    First q added

---
+++ 01_question.py
@@ -0,0 +1,35 @@
+"""
+Задача 22: 
+Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+Пользователь вводит 2 числа. 
+n — кол-во элементов первого множества. 
+m — кол-во элементов второго множества. 
+Затем пользователь вводит сами элементы множеств.
+
+"""
+import random
+
+first_list = []
+second_list = []
+
+n = int(input('Enter first length: '))
+m = int(input('Enter second length: '))
+
+print('Enter numbers for the first list:')
+for i in range(0, n):
+    first_list.append(input(f'{i + 1} element: '))
+
+print('Enter numbers for the second list:')
+for i in range(0, m):
+    second_list.append(input(f'{i + 1} element: '))
+
+print('First list = ' , first_list)
+print('Second list = ' , second_list)
+
+some_set = set(first_list).intersection(set(second_list))
+
+final_list = list(some_set)
+final_list.sort()
+
+print('Final result: ', final_list)
